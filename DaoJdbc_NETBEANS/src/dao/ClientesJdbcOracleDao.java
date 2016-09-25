264a67627c5077d660bbb4edc1203d6313848357
Lupercar <hotolete@gmail.com>
2016-09-25 12:38:44 +0200

    Procesamos la operacion correspondiente, en este caso devolvemos la lista

--- DaoJdbc_NETBEANS/src/dao/ClientesJdbcOracleDao.java
+++ DaoJdbc_NETBEANS/src/dao/ClientesJdbcOracleDao.java
@@ -5,22 +5,32 @@ import entidades.Cliente;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import oracle.jdbc.OracleDriver;
 
 public class ClientesJdbcOracleDao implements DaoClientes{
 
+    private Properties prop = new Properties();
+            
     public ClientesJdbcOracleDao() throws SQLException, IOException {
 //        Cargar driver a la MVJ
         DriverManager.registerDriver(new OracleDriver());
         
 //        Leemos configuración BBDD de un fichero de propiedades
-        Properties prop = new Properties();
         prop.load(ClientesJdbcOracleDao
                 .class
                 .getResourceAsStream("../configuracion/Oracle.properties"));
+    }
+    
+    @Override
+    public List<Cliente> get() throws SQLException {
+        
+        List<Cliente> clientes = new ArrayList<>(); 
 
 //        Creamos conexion a BBDD
         Connection conexion = DriverManager
@@ -28,11 +38,27 @@ public class ClientesJdbcOracleDao implements DaoClientes{
                         prop.getProperty("url"), 
                         prop.getProperty("user"), 
                         prop.getProperty("password"));
-    }
-    
-    @Override
-    public List<Cliente> get() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+//        Hacemos la operación correspondiente
+        Statement comando = conexion.createStatement(); 
+        String sql = "SELECT * FROM cliente"; 
+        ResultSet resultado = comando.executeQuery(sql); 
+        
+//        procesamos los datos y los pasamos a objetos
+        while (resultado.next()) {            
+//            Accedemos a las filas
+            int id = resultado.getInt("id");
+            String nombre = resultado.getString("nombre"); 
+            
+            Cliente c = new Cliente(id, nombre);
+            clientes.add(c); 
+        }
+
+        resultado.close();
+        comando.close();
+        conexion.close();
+        
+        return clientes; 
     }
 
     @Override
--- DaoJdbc_NETBEANS/src/dao/interfaces/Dao.java
+++ DaoJdbc_NETBEANS/src/dao/interfaces/Dao.java
@@ -1,9 +1,10 @@
 package dao.interfaces;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface Dao <T>{ 
-    List<T> get(); 
+    List<T> get()throws SQLException; 
     T get(int id);
     void add(T entidad); 
     void remove(T entidad); 
