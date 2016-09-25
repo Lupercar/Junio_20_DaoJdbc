eafe3e9ca0164f5c1799a3420241818efb20f686
Lupercar <hotolete@gmail.com>
2016-09-25 21:27:42 +0200

    Aplico Excepciones en cada método abstracto

--- DaoJdbc_NETBEANS/src/dao/interfaces/Dao.java
+++ DaoJdbc_NETBEANS/src/dao/interfaces/Dao.java
@@ -3,10 +3,10 @@ package dao.interfaces;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface Dao <T>{ 
+public interface Dao <T> { 
     List<T> get()throws SQLException; 
-    T get(int id);
-    void add(T entidad); 
-    void remove(T entidad); 
-    void update(T entidad); 
+    T get(int id)throws SQLException;
+    void add(T entidad)throws SQLException; 
+    void remove(T entidad)throws SQLException; 
+    void update(T entidad)throws SQLException; 
 }//fin interface dao.Dao
