054420b5547a622fed9912719edb7b97e0a8512a
Lupercar <hotolete@gmail.com>
2016-09-25 21:33:20 +0200

    Daremos funcionalidad al get de un único Cliente, creo un método para la conexión a la BBDD

--- DaoJdbc_NETBEANS/src/dao/ClientesJdbcOracleDao.java
+++ DaoJdbc_NETBEANS/src/dao/ClientesJdbcOracleDao.java
@@ -13,56 +13,78 @@ import java.util.List;
 import java.util.Properties;
 import oracle.jdbc.OracleDriver;
 
-public class ClientesJdbcOracleDao implements DaoClientes{
+public class ClientesJdbcOracleDao implements DaoClientes {
 
     private Properties prop = new Properties();
-            
+
     public ClientesJdbcOracleDao() throws SQLException, IOException {
 //        Cargar driver a la MVJ
         DriverManager.registerDriver(new OracleDriver());
-        
+
 //        Leemos configuración BBDD de un fichero de propiedades
-        prop.load(ClientesJdbcOracleDao
-                .class
+        prop.load(ClientesJdbcOracleDao.class
                 .getResourceAsStream("../configuracion/Oracle.properties"));
     }
-    
+
     @Override
     public List<Cliente> get() throws SQLException {
-        
-        List<Cliente> clientes = new ArrayList<>(); 
+
+        List<Cliente> clientes = new ArrayList<>();
 
 //        Creamos conexion a BBDD
         Connection conexion = DriverManager
                 .getConnection(
-                        prop.getProperty("url"), 
-                        prop.getProperty("user"), 
+                        prop.getProperty("url"),
+                        prop.getProperty("user"),
                         prop.getProperty("password"));
-        
-//        Hacemos la operación correspondiente
-        Statement comando = conexion.createStatement(); 
-        String sql = "SELECT * FROM cliente"; 
-        ResultSet resultado = comando.executeQuery(sql); 
-        
-//        procesamos los datos y los pasamos a objetos
-        while (resultado.next()) {            
 
-            clientes.add( new Cliente(
-                    resultado.getInt("id"), 
+//        Hacemos la operación correspondiente
+        Statement comando = conexion.createStatement();
+        String sql = "SELECT * FROM cliente";
+        ResultSet resultado = comando.executeQuery(sql);
+
+//        procesamos los datos y los pasamos a objetos
+        while (resultado.next()) {
+
+            clientes.add(new Cliente(
+                    resultado.getInt("id"),
                     resultado.getString("nombre")
-            )); 
+            ));
         }
 
         resultado.close();
         comando.close();
         conexion.close();
-        
-        return clientes; 
+
+        return clientes;
     }
 
     @Override
-    public Cliente get(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Cliente get(int id) throws SQLException {
+        Cliente cliente = null;
+
+//        Creamos conexion a BBDD
+        Connection conexion = this.getConexion(); 
+
+//        Hacemos la operación correspondiente
+        Statement comando = conexion.createStatement();
+        String sql = "SELECT * FROM cliente WHERE id =" + id;
+        ResultSet resultado = comando.executeQuery(sql);
+
+//        procesamos los datos y los pasamos a objetos
+        while (resultado.next()) {
+
+            clientes.add(new Cliente(
+                    resultado.getInt("id"),
+                    resultado.getString("nombre")
+            ));
+        }
+
+        resultado.close();
+        comando.close();
+        conexion.close();
+
+        return cliente;
     }
 
     @Override
@@ -80,4 +102,13 @@ public class ClientesJdbcOracleDao implements DaoClientes{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    private Connection getConexion() throws SQLException{
+//        Creamos conexion a BBDD
+        Connection conexion = DriverManager
+                .getConnection(
+                        prop.getProperty("url"),
+                        prop.getProperty("user"),
+                        prop.getProperty("password"));
+        return conexion; 
+    } 
 }//fin class dao.ClientesJdbcDao
